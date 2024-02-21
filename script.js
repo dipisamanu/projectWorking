00b5704af12de69f01886b42214d55e5450c9ca8
dipisamanu <10934011@itisrossi.vi.it>
2024-02-21 20:51:07 +0100

    ok

---
+++ script.js
@@ -0,0 +1,16 @@
+let row = 5;
+let col = 5;
+let array = [];
+
+for(let i=0; i<5;i++){
+    array[i] = [];
+}
+document.getElementById("grid").style.gridTemplateColumns = "repeat(" + col + ", 1fr)"
+document.getElementById("grid").style.gridTemplateRows = "repeat(" + row + ", 1fr)"
+
+for (let i = 0; i < (25); i++) {
+    let box = "<div class='box'></div>";
+    document.getElementById("grid").innerHTML += box;
+}
+
+console.log(array, array.length);
