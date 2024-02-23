59766409f258f96f53a55ff0dcf132d70cf7c40b
dipisamanu <10934011@itisrossi.vi.it>
2024-02-23 09:03:30 +0100

    immagini corrette

--- script.js
+++ script.js
@@ -5,14 +5,18 @@ let array = [];
 for (let i = 0; i < 5; i++) {
     array[i] = [];
 }
+
 document.getElementById("grid").style.gridTemplateColumns = "repeat(" + col + ", 1fr)"
 document.getElementById("grid").style.gridTemplateRows = "repeat(" + row + ", 1fr)"
 
+let imagePaths = ['bottle.jpeg', '/Users/ottaviodipisa/Documents/GitHub/projectWorking/cardboard.jpeg', '/Users/ottaviodipisa/Documents/GitHub/projectWorking/plastic.jpeg', '/Users/ottaviodipisa/Documents/GitHub/projectWorking/waste.jpeg']; 
+
+function getRandomImagePath() {
+    return imagePaths[Math.floor(Math.random() * imagePaths.length)];
+}
+
 for (let i = 0; i < (25); i++) {
-    let box = "<div class='box'></div>";
-    document.getElementById("grid").innerHTML += box;
+    let randomImagePath = getRandomImagePath();
+    let s = "<div class='box'><img src='" + randomImagePath + "' alt='Image'></div>";
+    document.getElementById("grid").innerHTML += s;
 }
-for (let i = 0; i < 25; i++) {
-    //inserire la cosa che ogni volta che genera la cella crea allinterno un numero random che identifica il tipo 
-}
-console.log(array, array.length);
