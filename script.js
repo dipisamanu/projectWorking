4e9dd84c266a20abc5613cd8308f31d874d78ba7
dipisamanu <10934011@itisrossi.vi.it>
2024-02-25 14:53:43 +0100

    griglia con matrici

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 let row = 5;
 let col = 5;
-let array = [];
-
-for (let i = 0; i < 5; i++) {
-    array[i] = [];
-}
+let grid = [];
 
 document.getElementById("grid").style.gridTemplateColumns = "repeat(" + col + ", 1fr)"
 document.getElementById("grid").style.gridTemplateRows = "repeat(" + row + ", 1fr)"
 
-let imagePaths = ['bottle.jpeg', '/Users/ottaviodipisa/Documents/GitHub/projectWorking/cardboard.jpeg', '/Users/ottaviodipisa/Documents/GitHub/projectWorking/plastic.jpeg', '/Users/ottaviodipisa/Documents/GitHub/projectWorking/waste.jpeg']; 
+let imagePaths = {
+    'bottle.png': 5,
+    'cardboard.png': 2,
+    'plastic.png': 3,
+    'waste.png': 1
+};
+
+let imageKeys = Object.keys(imagePaths);
 
 function getRandomImagePath() {
-    return imagePaths[Math.floor(Math.random() * imagePaths.length)];
+    return imageKeys[Math.floor(Math.random() * imageKeys.length)];
 }
 
-for (let i = 0; i < (25); i++) {
-    let randomImagePath = getRandomImagePath();
-    let s = "<div class='box'><img src='" + randomImagePath + "' alt='Image'></div>";
-    document.getElementById("grid").innerHTML += s;
+for (let i = 0; i < row; i++) {
+    grid[i] = [];
+    for (let j = 0; j < col; j++) {
+        let imagePath = getRandomImagePath();
+        let s = "<div class='box' data-row='" + i + "' data-col='" + j + "' value='" + imagePaths[imagePath] + "'><img src='" + imagePath + "' alt='Image'></div>";
+        document.getElementById("grid").innerHTML += s;
+        grid[i][j] = s;
+    }
+}
+let boxes = document.getElementsByClassName('box');
+for (let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener('click', function () {
+        let row = this.getAttribute('data-row');
+        let col = this.getAttribute('data-col');
+        let value = this.getAttribute('value');
+        console.log('Hai fatto clic sulla casella:', row, col, 'con valore:', value);
+    });
 }
