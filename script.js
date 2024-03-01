c10c96a915376c56267819423c2d6d24d66b0002
dipisamanu <10934011@itisrossi.vi.it>
2024-03-01 09:03:00 +0100

    Revert "gestione tris"
    This reverts commit

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 let row = 5;
 let col = 5;
 let grid = [];
-document.getElementById("grid").style.gridTemplateColumns = "repeat(" + col + ", 1fr)";
-document.getElementById("grid").style.gridTemplateRows = "repeat(" + row + ", 1fr)";
+
+document.getElementById("grid").style.gridTemplateColumns = "repeat(" + col + ", 1fr)"
+document.getElementById("grid").style.gridTemplateRows = "repeat(" + row + ", 1fr)"
+
 let percorsoImmagini = {
     'images/bottle.png': 5,
     'images/cardboard.png': 2,
     'images/plastic.png': 3,
     'images/waste.png': 1
 };
+
 let imageKeys = Object.keys(percorsoImmagini);
+
 function generaImmagine(i, j) {
     let imagePath;
     do {
@@ -17,6 +21,7 @@ function generaImmagine(i, j) {
     } while ((grid[i - 1] && grid[i - 1][j] === imagePath && grid[i - 2] && grid[i - 2][j] === imagePath) || (grid[i][j - 1] === imagePath && grid[i][j - 2] === imagePath));
     return imagePath;
 }
+
 for (let i = 0; i < row; i++) {
     grid[i] = [];
     for (let j = 0; j < col; j++) {
@@ -34,6 +39,7 @@ for (let i = 0; i < boxes.length; i++) {
         let col = parseInt(this.getAttribute('data-col'));
         let value = this.getAttribute('value');
         console.log('Hai fatto clic sulla casella:', row, col, 'con valore:', value);
+
         if (selBox != null) {
             let selRow = parseInt(selBox.getAttribute('data-row'));
             let selCol = parseInt(selBox.getAttribute('data-col'));
@@ -43,9 +49,6 @@ for (let i = 0; i < boxes.length; i++) {
                 grid[row][col] = temp;
                 selBox.getElementsByTagName('img')[0].src = grid[selRow][selCol];
                 this.getElementsByTagName('img')[0].src = grid[row][col];
-                while (checkThree()) {
-                    slideCandy();
-                }
             }
             selBox.classList.remove('selected');
             selBox = null;
@@ -54,7 +57,6 @@ for (let i = 0; i < boxes.length; i++) {
             selBox = this;
         }
     });
-
 }
 function crushCandy() {
     //crushFive();
