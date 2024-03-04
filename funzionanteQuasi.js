5cdd0dd12650da8e6fd72d3a2e589df52f1f51c8
dipisamanu <10934011@itisrossi.vi.it>
2024-03-04 22:11:29 +0100

    funzionanteQuasi migliorato

--- funzionanteQuasi.js
+++ funzionanteQuasi.js
@@ -1,44 +1,47 @@
 let row = 5;
 let col = 5;
 let grid = [];
-document.getElementById("grid").style.gridTemplateColumns = "repeat(" + col + ", 1fr)";
-document.getElementById("grid").style.gridTemplateRows = "repeat(" + row + ", 1fr)";
+let gridElement = document.getElementById("grid");
+gridElement.style.gridTemplateColumns = "repeat(" + col + ", 1fr)";
+gridElement.style.gridTemplateRows = "repeat(" + row + ", 1fr)";
+
 let percorsoImmagini = {
     'images/bottle.png': 5,
     'images/cardboard.png': 2,
     'images/plastic.png': 3,
-    'images/waste.png': 1
+    'images/waste.png': 1,
+    'images/blank.png': 0
 };
-let imageKeys = Object.keys(percorsoImmagini); //prende il riferimento della key di quell'oggetto
+
+let imageKeys = Object.keys(percorsoImmagini);
+
 function generaImmagine(i, j) {
     let imagePath;
     do {
-        imagePath = imageKeys[Math.floor(Math.random() * imageKeys.length)];
+        imagePath = imageKeys[Math.floor(Math.random() * (imageKeys.length - 1))]; // escludiamo l'ultimo elemento che è 'images/blank.png'
     } while ((grid[i - 1] && grid[i - 1][j] === imagePath && grid[i - 2] && grid[i - 2][j] === imagePath) || (grid[i][j - 1] === imagePath && grid[i][j - 2] === imagePath));
     return imagePath;
 }
-for (let i = 0; i < row; i++) {
-    grid[i] = [];
-    for (let j = 0; j < col; j++) {
-        let imagePath = generaImmagine(i, j);
-        let s = "<div class='box' data-row='" + i + "' data-col='" + j + "' value='" + percorsoImmagini[imagePath] + "'><img src='" + imagePath + "' alt='Image'></div>";
-        document.getElementById("grid").innerHTML += s;
-        grid[i][j] = imagePath;
+
+
+function creaGriglia() {
+    for (let i = 0; i < row; i++) {
+        grid[i] = [];
+        for (let j = 0; j < col; j++) {
+            let imagePath = generaImmagine(i, j);
+            let s = "<div class='box' data-row='" + i + "' data-col='" + j + "' value='" + percorsoImmagini[imagePath] + "'><img src='" + imagePath + "' alt='Image'></div>";
+            gridElement.innerHTML += s;
+            grid[i][j] = imagePath;
+        }
     }
 }
-let boxes = document.getElementsByClassName('box');
-let selBox = null;
+
 function checkForAlignments() {
     let removed = false;
     for (let i = 0; i < row; i++) {
         for (let j = 0; j < col - 2; j++) {
             if (grid[i][j] === grid[i][j + 1] && grid[i][j] === grid[i][j + 2]) {
-                document.getElementsByClassName('box')[i * col + j].getElementsByTagName('img')[0].src = '';
-                document.getElementsByClassName('box')[i * col + j + 1].getElementsByTagName('img')[0].src = '';
-                document.getElementsByClassName('box')[i * col + j + 2].getElementsByTagName('img')[0].src = '';
-                grid[i][j] = null;
-                grid[i][j + 1] = null;
-                grid[i][j + 2] = null;
+                rimuoviCelle(i, j, 'orizzontale');
                 removed = true;
             }
         }
@@ -46,42 +49,7 @@ function checkForAlignments() {
     for (let j = 0; j < col; j++) {
         for (let i = 0; i < row - 2; i++) {
             if (grid[i][j] === grid[i + 1][j] && grid[i][j] === grid[i + 2][j]) {
-                document.getElementsByClassName('box')[i * col + j].getElementsByTagName('img')[0].src = '';
-                document.getElementsByClassName('box')[(i + 1) * col + j].getElementsByTagName('img')[0].src = '';
-                document.getElementsByClassName('box')[(i + 2) * col + j].getElementsByTagName('img')[0].src = '';
-                grid[i][j] = null;
-                grid[i + 1][j] = null;
-                grid[i + 2][j] = null;
-                removed = true;
-            }
-        }
-    }
-    return removed;
-}
-function checkBoard() {
-    let removed = false;
-    for (let i = 0; i < row; i++) {
-        for (let j = 0; j < col - 2; j++) {
-            if (grid[i][j] === grid[i][j + 1] && grid[i][j] === grid[i][j + 2]) {
-                document.getElementsByClassName('box')[i * col + j].getElementsByTagName('img')[0].src = '';
-                document.getElementsByClassName('box')[i * col + j + 1].getElementsByTagName('img')[0].src = '';
-                document.getElementsByClassName('box')[i * col + j + 2].getElementsByTagName('img')[0].src = '';
-                grid[i][j] = null;
-                grid[i][j + 1] = null;
-                grid[i][j + 2] = null;
-                removed = true;
-            }
-        }
-    }
-    for (let j = 0; j < col; j++) {
-        for (let i = 0; i < row - 2; i++) {
-            if (grid[i][j] === grid[i + 1][j] && grid[i][j] === grid[i + 2][j]) {
-                document.getElementsByClassName('box')[i * col + j].getElementsByTagName('img')[0].src = '';
-                document.getElementsByClassName('box')[(i + 1) * col + j].getElementsByTagName('img')[0].src = '';
-                document.getElementsByClassName('box')[(i + 2) * col + j].getElementsByTagName('img')[0].src = '';
-                grid[i][j] = null;
-                grid[i + 1][j] = null;
-                grid[i + 2][j] = null;
+                rimuoviCelle(i, j, 'verticale');
                 removed = true;
             }
         }
@@ -89,26 +57,48 @@ function checkBoard() {
     return removed;
 }
 
+function rimuoviCelle(i, j, direzione) {
+    let celleDaRimuovere = direzione === 'orizzontale' ? [[i, j], [i, j + 1], [i, j + 2]] : [[i, j], [i + 1, j], [i + 2, j]];
+    celleDaRimuovere.forEach(([row, col]) => {
+        let box = document.getElementsByClassName('box')[row * col + col];
+        box.getElementsByTagName('img')[0].src = 'images/blank.png';
+        box.setAttribute('value', percorsoImmagini['images/blank.png']); // Aggiorna il valore della cella a 0
+        grid[row][col] = 'images/blank.png';
+    });
+}
+
 function dropCells() {
     for (let j = 0; j < col; j++) {
         let emptyRows = [];
         for (let i = row - 1; i >= 0; i--) {
-            if (!grid[i][j]) {
+            if (grid[i][j] === 'images/blank.png') {
                 emptyRows.push(i);
             } else if (emptyRows.length > 0) {
                 let newRow = emptyRows.pop();
+                let box = document.getElementsByClassName('box')[newRow * col + j];
+                box.getElementsByTagName('img')[0].src = grid[i][j];
+                box.setAttribute('value', percorsoImmagini[grid[i][j]]);
                 grid[newRow][j] = grid[i][j];
-                grid[i][j] = null;
-                document.getElementsByClassName('box')[newRow * col + j].getElementsByTagName('img')[0].src = grid[newRow][j];
-                document.getElementsByClassName('box')[i * col + j].getElementsByTagName('img')[0].src = '';
+                grid[i][j] = 'images/blank.png';
+                box = document.getElementsByClassName('box')[i * col + j];
+                box.getElementsByTagName('img')[0].src = 'images/blank.png';
+                box.setAttribute('value', percorsoImmagini['images/blank.png']);
             }
         }
         for (let i = 0; i < emptyRows.length; i++) {
             grid[emptyRows[i]][j] = generaImmagine(emptyRows[i], j);
-            document.getElementsByClassName('box')[emptyRows[i] * col + j].getElementsByTagName('img')[0].src = grid[emptyRows[i]][j];
+            let box = document.getElementsByClassName('box')[emptyRows[i] * col + j];
+            box.getElementsByTagName('img')[0].src = grid[emptyRows[i]][j];
+            box.setAttribute('value', percorsoImmagini[grid[emptyRows[i]][j]]);
         }
     }
 }
+
+
+creaGriglia();
+
+let boxes = document.getElementsByClassName('box');
+let selBox = null;
 
 for (let i = 0; i < boxes.length; i++) {
     boxes[i].addEventListener('click', function () {
@@ -125,7 +115,7 @@ for (let i = 0; i < boxes.length; i++) {
                 grid[row][col] = temp;
                 selBox.getElementsByTagName('img')[0].src = grid[selRow][selCol];
                 this.getElementsByTagName('img')[0].src = grid[row][col];
-                while (checkBoard()) {
+                while (checkForAlignments()) {
                     dropCells();
                 }
             }
@@ -136,5 +126,4 @@ for (let i = 0; i < boxes.length; i++) {
             selBox = this;
         }
     });
-
 }
