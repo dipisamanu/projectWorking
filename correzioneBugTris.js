79a64b608ce787876f528c6fd0e8307967e7b073
dipisamanu <10934011@itisrossi.vi.it>
2024-03-04 19:05:05 +0100

    continuo correzione codice

--- correzioneBugTris.js
+++ correzioneBugTris.js
@@ -1,34 +1,18 @@
-let types = ["bottle", "waste", "cardboard", "glass", "plastic"]
-let board = [];
+let oggetti = ["waste", "cardboard", "glass", "plastic"];
 let rows = 5;
 let columns = 5;
+let grid = [];
 
-window.onload = function () { //quando viene caricata la pagina
-    startGame();
-    //ogni decimo di secondo 
-    window.setInterval(function () {
-        // crushCandy();
-        // slideCandy();
-        // generateCandy();
-    }, 100);
+function random(i, j) {
+    let randomImage = oggetti[Math.floor(Math.random() * 4)];
+    return randomImage;
 }
-
-function randomCell() {
-    return types[Math.floor(Math.random() * types.length)]; //0 - 4.99
-}
-
-function startGame() {
-    for (let r = 0; r < rows; r++) {
-        let row = [];
-        for (let c = 0; c < columns; c++) {
-            // <img id="0-0" src="./images/Waste.png">
-            let tile = document.createElement("img"); //crea un elemento immagine
-            tile.id = r.toString() + "-" + c.toString(); //all'id della cella aggiungo il numero di row e col
-            tile.src = "images/" + randomCell() + ".png"; //gli attribuisco una random image
-            row.push(tile);
+function genera() {
+    for (let i = 0; i < rows; i++) {
+        grid[i] = [];
+        for (let j = 0; j < rows; j++) { 
+            
         }
-        board.push(row);
-    }
-    console.log(board);
 
+    }
 }
